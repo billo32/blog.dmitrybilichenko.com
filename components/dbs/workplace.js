dd515f81f02e94a0f82bd3983ca5320f8e74d4b8
Dmitry Bilichenko <Dmitry.Bilichenko@kaspersky.com>
2023-07-27 01:34:49 +0300

    Updates for Workplace component

--- components/dbs/workplace.js
+++ components/dbs/workplace.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Link from 'next/link'
 import classes from './workplace.module.css'
 
 const WorkPlace = (props) => {
@@ -7,9 +7,10 @@ const WorkPlace = (props) => {
     <li className={classes['work-place']}>
       <div className={classes.title}>{props.title}</div>
       <div className={classes.subtitle}>{props.subtitle}</div>
-      <a href={props.link} target="_blank" rel="noreferrer">
+
+      <Link key={props.linkTitle} href={props.link} target="_blank">
         {props.linkTitle}
-      </a>
+      </Link>
     </li>
   )
 }
